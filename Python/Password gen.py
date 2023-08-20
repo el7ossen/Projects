2fe7417071a466d1f060a384841a51d23bc85715
el7ossen <husainadwan0@gmail.com>
2023-08-20 06:08:56 +0300

    final compact version 1.2

--- Python/Password gen.py
+++ Python/Password gen.py
@@ -1,16 +1,11 @@
 import string, random
-
 while True:
     try:
-        len = int(input("Choose the length of your password: "))
+        length = int(input("Choose the length of your password: "))
         break
-
     except ValueError:
         print("Please input a number! \n")
         continue
-
-char = string.ascii_letters + string.digits + "!@#$-_"
-
-password = random.sample(char, len)
-
+char = ((string.ascii_letters + string.digits + "!@#$-_") * 10000)
+password = random.sample(char, length)
 print("".join(password))
