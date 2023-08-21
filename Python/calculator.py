fd7ff6bad5ec08ea3b48fa26522dbeef15563cd8
el7ossen <husainadwan0@gmail.com>
2023-08-21 12:41:23 +0300

    calculator 1.1

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,10 +1,14 @@
-import math, decimal, fractions
+import math, fractions
+from decimal import Decimal
 
 while True:
     print(
         "\nChoose an operation:\n\n",
         "Quit\n",
         "1 Addition\n",
+        "2 Subtraction\n",
+        "3 Multiplication\n",
+        "4 Division\n",
     )
 
     op = str(input("Option: "))
@@ -14,12 +18,33 @@ while True:
 
     elif op == "1":
         print("Number A is? ")
-        A = decimal.Decimal(input())
+        A = Decimal(input())
         print("Number B is? ")
-        B = decimal.Decimal(input())
+        B = Decimal(input())
         Add = A + B
         print("Your answer is ", Add)
+
+    elif op == "2":
+        print("Number A is? ")
+        A = Decimal(input())
+        print("Number B is? ")
+        B = Decimal(input())
+        Sub = A - B
+        print("Your answer is ", Sub)
     
+    elif op == "3":
+        print("Number A is? ")
+        A = Decimal(input())
+        print("Number B is? ")
+        B = Decimal(input())
+        Mult = A * B
+        print("Your answer is ", Mult)
+
+    elif op == "4":
+        print("Number A is? ")
+        A = Decimal(input())
+
+
     ret = str(input("Would you like to retry? y/n "))
     if ret == "y":
         continue
