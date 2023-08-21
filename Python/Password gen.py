55dd5a2aa4bfec5031352ff7ac5e6671c9137027
el7ossen <husainadwan0@gmail.com>
2023-08-22 00:06:30 +0300

    v1.3 negative number and list limit fixes

--- Python/Password gen.py
+++ Python/Password gen.py
@@ -1,11 +1,14 @@
 import string, random
+char = ((string.ascii_letters + string.digits + "!@#$-_"))
 while True:
     try:
         length = int(input("Choose the length of your password: "))
-        break
     except ValueError:
-        print("Please input a number! \n")
+        print("Please input a positive number! \n")
         continue
-char = ((string.ascii_letters + string.digits + "!@#$-_") * 10000)
-password = random.sample(char, length)
+    if length > 0: break
+    else:
+        print("Please input a postitive number!\n") 
+        continue
+password = random.choices(char, k=length)
 print("".join(password))
