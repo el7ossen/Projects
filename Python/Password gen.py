fa35be3a7240646904c23ed779c23c98d3cdffc2
el7ossen <husainadwan0@gmail.com>
2023-08-25 10:40:07 +0300

    v1.6 different error messages

--- Python/Password gen.py
+++ Python/Password gen.py
@@ -1,14 +1,14 @@
 import string, random, pyperclip
-char = ((string.ascii_letters + string.digits + "!@#$-_"))
+char = (string.ascii_letters + string.digits + "!@#$-_")
 while True:
     try:
         length = int(input("Choose the length of your password: "))
     except ValueError:
-        print("Please input a positive number! \n")
+        print("Please input a positive integer! \n")
         continue
     if length > 0: break
     else:
-        print("Please input a postitive number!\n") 
+        print("Please input a postitive integer!\n") 
         continue
 passw = random.choices(char, k=length)
 
