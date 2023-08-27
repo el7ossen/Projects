b5f570a854b066e403dd9a30413b09e013d4ea8a
el7ossen <husainadwan0@gmail.com>
2023-08-28 02:08:54 +0300

    v1.1 diff path

--- password manager/decrypt.py
+++ password manager/decrypt.py
@@ -1,17 +1,17 @@
 from cryptography.fernet import Fernet
 
 def dec():
-    with open("filekey.key", "rb") as filekey:
+    with open("txt/filekey.key", "rb") as filekey:
         key = filekey.read()
 
     fernet = Fernet(key)
 
-    with open("plist.txt", "rb") as enc_file:
+    with open("txt/plist.txt", "rb") as enc_file:
         encrypted = enc_file.read()
 
     decrypted = fernet.decrypt(encrypted)
 
-    with open("plist.txt", "wb") as dec_file:
+    with open("txt/plist.txt", "wb") as dec_file:
         dec_file.write(decrypted)
     dec_file.close()
 
