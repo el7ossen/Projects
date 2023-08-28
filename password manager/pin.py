414260b9223e097b5c69f589d55a0b916da40ed4
el7ossen <husainadwan0@gmail.com>
2023-08-28 21:30:35 +0300

    first commit

---
+++ password manager/pin.py
@@ -0,0 +1,14 @@
+importos
+
+i = 0
+while i < 5:
+    pin = input("Please neter your pin: ")
+    if pin == "0000":
+        print("yes")
+        break
+    else:
+        i += 1
+        print("no")
+
+if i == 5:
+    os.remove(__file__)
