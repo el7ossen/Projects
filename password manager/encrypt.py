f2eb62fd4ce539a7abc8b693ba9836b9f4d002a4
el7ossen <husainadwan0@gmail.com>
2023-08-28 04:28:58 +0300

    v1.3

--- password manager/encrypt.py
+++ password manager/encrypt.py
@@ -2,17 +2,15 @@ from cryptography.fernet import Fernet
 
 def enc():
 
-    with open('txt/filekey.key', 'rb') as filekey:
-        key = filekey.read()
 
-    fernet = Fernet(key)
+    fernet = Fernet("98ynLvTdueiIo9oERYwLDrJc4HqzZwTh7ES1QO75iHg=")
 
-    with open('txt/plist.txt', 'rb') as file:
+    with open('plist.txt', 'rb') as file:
         original = file.read()
 
     encrypted = fernet.encrypt(original)
 
-    with open('txt/plist.txt', 'wb') as encrypted_file:
+    with open('plist.txt', 'wb') as encrypted_file:
         encrypted_file.write(encrypted)
     encrypted_file.close()
 
