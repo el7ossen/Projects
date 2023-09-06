97a46d261c03b47189341180642ebb6af941275d
el7ossen <husainadwan0@gmail.com>
2023-09-06 22:46:26 +0300

    v1.2

--- CS50P/itunes.py
+++ CS50P/itunes.py
@@ -2,11 +2,6 @@
 
 import requests, sys, json
 
-if len(sys.argv) != 2:
-    sys.exit("failed")
+response = requests.get("http://localhost:8080/index/subsites/test.txt")
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
-
-o = response.json()
-for result in o["results"]:
-    print(result["trackName"])
+print(response.text)
