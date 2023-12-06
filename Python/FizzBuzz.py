2aa6b54607b8986b7f194f75f3e1b62d4a664ce1
el7ossen <husainadwan0@gmail.com>
2023-12-06 14:12:53 +0300

    v1.1

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -2,8 +2,10 @@ for x in range(1,101):
 
     output=""
 
-    if x%3==0: output + "Fizz"
-    if x%5==0: output + "Buzz"
+    if x%3==0: 
+        output = output + "Fizz"
+    if x%5==0: 
+        output = output + "Buzz"
 
     if output=="": print(x)
 
