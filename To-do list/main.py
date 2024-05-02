2531ffae6940d5c226059c763fd6f2a880debea9
el7ossen <husainadwan0@gmail.com>
2024-05-02 15:55:18 +0300

    v1.3

--- To-do list/main.py
+++ To-do list/main.py
@@ -3,14 +3,13 @@ import pandas as pd
 
 path = os.path.abspath(os.path.dirname(__file__))
 filename = "todo.csv"
-oid = pd.read_csv(path + "/todo.csv").iloc[-1,0]
 filepath = f"{path}/{filename}"
 
 
 def newtask():
     task = input("What's your task?")
     dd = input("What's the due date?")
-    data = {'ID':[oid+1] , 'Task':[task] , 'DD':[dd] , 'Progress':["nd"]}
+    data = {'Task':[task] , 'DD':[dd] , 'Progress':["nd"]}
     df = pd.DataFrame(data)
     df.to_csv(filepath, mode='a', index=False, header=False)
 
@@ -18,4 +17,5 @@ def readtask():
     df = pd.read_csv(filepath)
     print(df.shape[0])
 
+newtask()
 readtask()
