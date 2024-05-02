69898f593e364cc40e219da50b4675e0d639961a
el7ossen <husainadwan0@gmail.com>
2024-05-02 15:55:54 +0300

    first commit

---
+++ To-do list/checktask.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+path = os.path.abspath(os.path.dirname(__file__))
+filename = "todo.csv"
+filepath = f"{path}/{filename}"
+checknd = ['nd']
+checkd = ["d"]
+
+def checktask():
+    checked
+
+checktask()
