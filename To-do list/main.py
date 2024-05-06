61adbaceacafd8c574382616d2f225ce3cebf9ec
el7ossen <husainadwan0@gmail.com>
2024-05-07 00:15:15 +0300

    v1.5

--- To-do list/main.py
+++ To-do list/main.py
@@ -1,36 +1,47 @@
-import pathlib
+from pathlib import Path
 import pandas as pd
 
-path = pathlib.Path.cwd()
-filename = "todo.csv"
-filepath = f"{path}/{filename}"
+
+#constants
+path = Path.cwd()
+filepath = f"{path}/todo.csv"
 checknd = ['nd']
 checkd = ['d']
 df = pd.read_csv(filepath)
+Desc = "None"
+#---------- 
 
+#Functions
 def newtask():
-    task = input("What's your task?")
-    dd = input("What's the due date?")
-    data = {'Task':[task] , 'DD':[dd] , 'Progress':["nd"]}
+    task = input("What's your task? ")
+    dd = input("What's the due date? ")
+    Desc = input("Describtion? ")
+    data = {'Task':[task] , 'DD':[dd] , 'Progress':["nd"] , 'Desc':[Desc]}
     df = pd.DataFrame(data)
     df.to_csv(filepath, mode='a', index=False, header=False)
-
 def readtask():
-    df = pd.read_csv(filepath)
     print(df.shape[0])
-
-def dtasks():
-    checked = pd.DataFrame(pd.read_csv(filepath))['Progress'].isin(checkd)
-    print(pd.DataFrame(pd.read_csv(filename))[checked])
-
-def ndtasks():
-    checked = pd.DataFrame(pd.read_csv(filepath))['Progress'].isin(checknd)
-    print(pd.DataFrame(pd.read_csv(filename))[checked])
-    return checked
-
+def dtask():
+    checked = pd.DataFrame(df)['Progress'].isin(checkd)
+    print(pd.DataFrame(pd.read_csv(filepath))[checked])
+def ndtask():
+    checked = pd.DataFrame(df)['Progress'].isin(checknd)
+    ndf = pd.DataFrame(pd.read_csv(filepath))[checked]
+    print(ndf.to_markdown(tablefmt="pretty", stralign="left", index=None))
 def dotask():
-    ndtasks()
+    ndtask()
     while True:
-        op = str(input("\nwhich task would you like to do? "))
+        op = int(input("\nwhich task would you like to do? "))
+        try:
+            opdf = df.iloc[[op]]
+        except:
+            print("That's not an option!")
+            continue
+        df.loc[op, 'Progress'] = "d"
+        df.to_csv(filepath, index=False)
+        print("Done.")
+        break
+#----------
 
-dotask()
+#Program
+ndtask()
