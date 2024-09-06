4697b62ae9c2db53eec2a3a7191c3d8cf9bef96e
el7ossen <husainadwan0@gmail.com>
2024-09-06 16:19:32 +0300

    v1.1

--- go/go.mod
+++ go/go.mod
@@ -2,7 +2,10 @@ module example.com/mod
 
 go 1.23.0
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/01-edu/z01 v0.1.0
+	rsc.io/quote v1.5.2
+)
 
 require (
 	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
--- go/helloworld.go
+++ go/helloworld.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	hello := "Hello, cruel world."
+
+	// for i := 0; i < len(hello); i++ {
+	// 	z01.PrintRune(rune(hello[i]))
+	// }
+
+	for i, r := range hello {
+		z01.PrintRune(rune(r))
+		z01.PrintRune(' ')
+		fmt.Println(i)
+	}
+
+	// fmt.Println(quote.Go())
+	// z01.PrintRune('\n')
 }
